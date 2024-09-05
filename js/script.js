7976807a663b85f0988cea23db9296a76df96e2a
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-05 13:38:27 +0200

    MILESTONE 1

--- js/script.js
+++ js/script.js
@@ -3,6 +3,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            todoList: [
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+                {
+                    text: "Lavare i piatti",
+                    done: true
+                },
+                {
+                    text: "Finire il progetto",
+                    done: false
+                },
+                {
+                    text: "Allenarsi in palestra",
+                    done: true
+                }
+            ],
         }
     }
 }).mount('#app')
